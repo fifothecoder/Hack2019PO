1782fe52a16e75b3e1798f75d6c897d8f0de70c2
Andrej Kapusta <andrej.kapusta@studentstc.sk>
2019-10-04 17:42:16 +0200

    New Controller/View

---
+++ Hack2019PO/Controllers/MapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hack2019PO.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+    }
+}
