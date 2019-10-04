3ede1afcec8f46ed267db1d18b0e6d78668fb50b
Andrej Kapusta <andrej.kapusta@studentstc.sk>
2019-10-04 17:27:34 +0200

    Changes

--- Hack2019PO/Models/VoteDistrict.cs
+++ Hack2019PO/Models/VoteDistrict.cs
@@ -8,13 +8,13 @@ namespace Hack2019PO.Models
     public class VoteDistrict
     {
         public string Address;
-        public int AddressNo;
+        public string AddressNo;
         public string Venue;
         public string DistricAddress;
         public int DistricNo;
 
 
-        public VoteDistrict(string address, int addressno,string venue, string districtAddress, int districtNo)
+        public VoteDistrict(string address, string addressno,string venue, string districtAddress, int districtNo)
         {
             Address = address;
             AddressNo = addressno;
