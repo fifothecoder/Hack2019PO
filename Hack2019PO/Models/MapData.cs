b3ce483877631ce7b94b865d07c4a1bbd64da64c
Andrej Kapusta <andrej.kapusta@studentstc.sk>
2019-10-04 18:18:50 +0200

    MOdelll

---
+++ Hack2019PO/Models/MapData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hack2019PO.Models
+{
+    public class MapData
+    {
+        public string Address;
+        public string AddressNo;
+        
+    }
+}
