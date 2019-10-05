e90c66bfd1418dbc977ad371226a491c50042a63
Filip Biľ <filip112b@gmail.com>
2019-10-05 16:22:10 +0200

    push

---
+++ DbAdmin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DbAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "stcmicrosoft";
+            string password = "MatejPenis123";
+            string connectionstring = "";
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+
+            }
+
+            Console.WriteLine("Connection open");
+            Console.ReadLine();
+        }
+
+
+
+        
+    }
+}
